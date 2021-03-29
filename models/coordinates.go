ebe4d0abf2823fc0d27eff31320c356efbb46c22
Aitor <aitoraw93@gmail.com>
2021-03-29 13:12:49 +0200

    nomenclature

--- models/coordinates.go
+++ models/coordinates.go
@@ -1,6 +1,6 @@
 package models
 
-type Coordenadas struct {
+type Coordinates struct {
 	Coord struct {
 		Lon float64 `json:"lon"`
 		Lat float64 `json:"lat"`
--- service/city.go
+++ service/city.go
@@ -11,18 +11,18 @@ import (
 	"github.com/axi93/myWeather/models"
 )
 
-func Check() (models.Coordenadas, error) {
+func Check() (models.Coordinates, error) {
 	city := os.Args[1]
 	body := api.ObtainWeather(city)
 
 	if strings.Contains(body, "404") {
-		return models.Coordenadas{}, errors.New(" city not found, please enter a valid city ")
+		return models.Coordinates{}, errors.New(" city not found, please enter a valid city ")
 	}
 
 	return convertJson(body)
 }
 
-func convertJson(body string) (models.Coordenadas, error) {
+func convertJson(body string) (models.Coordinates, error) {
 	replace1 := strings.ReplaceAll(body, "test(", "")
 	body = strings.ReplaceAll(replace1, ")", "")
 	body = strings.ReplaceAll(body, "[", "")
@@ -30,16 +30,16 @@ func convertJson(body string) (models.Coordenadas, error) {
 
 	var jsonWeather = []byte(body)
 
-	var coordenates models.Coordenadas
+	var coordenates models.Coordinates
 	err := json.Unmarshal(jsonWeather, &coordenates)
 	if err != nil {
-		return models.Coordenadas{}, err
+		return models.Coordinates{}, err
 	}
 
 	return coordenates, nil
 }
 
-func ObtainSports(c models.Coordenadas) {
+func ObtainSports(c models.Coordinates) {
 	if c.Main.Temp >= 15 && c.Main.Temp <= 19.99 {
 		fmt.Println("You can do exercise outdoor")
 	} else if c.Main.Temp >= 10 && c.Main.Temp <= 14.99 {
